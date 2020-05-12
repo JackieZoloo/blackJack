54f643d19391f08308a2d4e0926f18c23709e073
jackiezoloo <62490560+JackieZoloo@users.noreply.github.com>
2020-05-12 16:56:51 -0600

    almost done with javascript

--- js/main.js
+++ js/main.js
@@ -8,18 +8,17 @@ let playerScore = document.getElementById("pScore");
 const dealerScore = document.getElementById("dScore");
 const playerCards = document.getElementById("playerCards");
 const dealerCards = document.getElementById("dealerCards");
-let playerPoint = 0;
+let message = document.getElementById("playerWin");
 let dealerHand = [];
 let playerHand = [];
 let pTotal = 0;
 let dTotal = 0;
-
+hit.disabled = false;
 
 // events 
 start.addEventListener("click", randomCards);
-
 hit.addEventListener("click", hitCard);
-stand.addEventListener("click", stand);
+stand.addEventListener("click", standButton);
 
 // Generate Rondam cards
 const suits = ["S", "D", "C", "H"];
@@ -27,60 +26,63 @@ const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"
 let score = 0;
 
 cards = [];
-suits.forEach(function(suit){                              // Going through suit which are S, D, C, H.
-    values.forEach(function(value){                        // Another function which is going through values.
-       const comCards = `${value}${suit}`; 
-       if(value === "A" && (pTotal < 15 || dTotal < 15)){
-           score = 11;
-       } else if (value === "A" && (pTotal > 15 || dTotal > 15)) {
-           score = 1;
-       } else if (value === "J" || value === "Q" || value === "K") {
-           score = 10;
-       } else {
-           score = parseInt(value);
-       }
-                                                           // Concatenates value and suit.
-       const card = {                                      // Creating an object for each cards.
-           imagePath: `images/cards/${comCards}.png`,   // Connecting image path to actual image.
-           value: value,
-           suit: suit,
-           score: score
-       }
-       cards.push(card);                                    // Creating card object to cards array.
+    suits.forEach(function(suit){                              // Going through suit which are S, D, C, H.
+        values.forEach(function(value){                        // Another function which is going through values.
+           const comCards = `${value}${suit}`; 
+           if(value === "A" && (pTotal < 15 || dTotal < 15)){
+               score = 11;
+           } else if (value === "A" && (pTotal > 15 || dTotal > 15)) {
+               score = 1;
+           } else if (value === "J" || value === "Q" || value === "K") {
+               score = 10;
+           } else {
+               score = parseInt(value);
+           }
+                                                               // Concatenates value and suit.
+           const card = {                                      // Creating an object for each cards.
+               imagePath: `images/cards/${comCards}.png`,   // Connecting image path to actual image.
+               value: value,
+               suit: suit,
+               score: score
+           }
+           cards.push(card);                                    // Creating card object to cards array.
+        })
     })
-})
+
 
 //Generating Random Cards
 
 function randomCards(){
-    clearcontent();
+    clearContent();
     let randomNum = Math.floor(Math.random() * cards.length);
     let numTwo = Math.floor(Math.random() * cards.length);
     let numThree = Math.floor(Math.random() * cards.length);
     playerHand.push(cards[randomNum], cards[numTwo]);
     dealerHand.push(cards[numThree]);
-    console.log(playerHand);
     startGame();
 }
-function clearcontent() { 
+function clearContent() { 
     playerHand = [];
     dealerHand = [];
-    console.log(playerHand);
+    pTotal = 0;
+    dTotal = 0;
+    hit.disabled = false;
+    stand.diabled = false;
 } 
 // start the game when start button is clicked
 function startGame(){
     playerCards.innerHTML = "";
+    message.innerText = " ";
     for( i = 0; i < playerHand.length; i++) {
         let firstCard = document.createElement("img");
         firstCard.setAttribute("src", playerHand[i].imagePath);
         playerCards.appendChild(firstCard);
     }
     dealerCards.innerHTML = "";
-
     for( i = 0; i < dealerHand.length; i++) {
         let backOfCard = document.createElement("img");
         backOfCard.setAttribute("src", "images/cards/blue_back.png");
-        playerCards.appendChild(backOfCard);
+        dealerCards.appendChild(backOfCard);
         let secondCard = document.createElement("img");
         secondCard.setAttribute("src", dealerHand[i].imagePath);
         dealerCards.appendChild(secondCard);
@@ -96,9 +98,19 @@ function checkScore(){
     for( i = 0; i < playerHand.length; i++){
         pTotal += playerHand[i].score;
     }
+    if(pTotal === 21) {
+        playerWin.innerHTML ="BlackJack";
+        hit.disabled = true;
+        stand.disabled = true;
+    }
     dTotal = 0;
     for( j = 0; j < dealerHand.length; j++){
         dTotal += dealerHand[j].score;
+    }
+    if(dTotal === 21) {
+        playerWin.innerHTML ="Dealer BlackJack";
+        hit.disabled = true;
+        stand.disabled = true;
     }
     playerScore.innerHTML = pTotal;
     dealerScore.innerHTML = dTotal;
@@ -115,6 +127,55 @@ function hitCard(){
     let newCard = document.createElement("img");
     newCard.setAttribute("src", cards[randomNum].imagePath);
     playerCards.appendChild(newCard);
+
+    playerHand.forEach(function(card){
+        if(card.value === "A") {
+            if(pTotal < 11){
+                card.scorec = 11;
+            } else {
+                card.score = 1;
+            }
+        }
+
+    })
     pTotal += cards[randomNum].score;
     playerScore.innerHTML = pTotal;
+    if(pTotal > 21) {
+        message.innerText = "You busted";
+        hit.disabled = true;
+    } else if(pTotal === 21) {
+        message.innerText = "You Won";
+        hit.disabled = true;
+    }
+    console.log(playerHand);
+}
+
+// stand button clicked 
+function standButton(){
+    while(dTotal < 18) {
+    let randomNum = Math.floor(Math.random() * cards.length);
+    dealerHand.push(cards[randomNum]);
+    let newCard = document.createElement("img");
+    newCard.setAttribute("src", cards[randomNum].imagePath);
+    dealerCards.appendChild(newCard);
+    playerHand.forEach(function(card){
+        if(card.value === "A") {
+            if(pTotal < 11){
+                card.scorec = 11;
+            } else {
+                card.score = 1;
+            }
+        }
+
+    })
+    dTotal += cards[randomNum].score;
+    dealerScore.innerHTML = dTotal;
+    }
+    if(dTotal > 21) {
+        message.innerText = "Dealer busted";
+    } else if(pTotal > dTotal && pTotal <= 21){
+        message.innerText = "You Won";
+    } else {
+        message.innerText = "Dealer Won";
+    }
 }
